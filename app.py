e62c1b991173e8a4ad92dc57a68a21a4f17cee71
EC2 Default User <ec2-user@ip-172-31-82-32.ec2.internal>
2025-12-05 08:19:06 +0000

    Remove debug mode

--- app.py
+++ app.py
@@ -481,4 +481,4 @@ def fetchdata():
 # ENTRY POINT
 # ----------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
